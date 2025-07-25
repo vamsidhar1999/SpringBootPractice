60bb21d90e2deb7ed5fe8730bb45b86392d89b7a
vamsidhar1999 <vamsidhar1999@gmail.com>
2025-07-26 01:29:40 +0530

    Project Setup

---
+++ demo/src/main/java/com/practice/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.practice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+		Dev dev = context.getBean(Dev.class);
+		dev.build();
+	}
+
+}
---
+++ demo/src/main/java/com/practice/demo/Dev.java
@@ -0,0 +1,11 @@
+package com.practice.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build(){
+        System.out.println("yes");
+    }
+}
